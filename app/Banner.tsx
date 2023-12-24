77efebf2520843591001e4c98a7e13b83f9448ec
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-12-24 15:29:10 +0800

    Update banner

--- app/Banner.tsx
+++ app/Banner.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import { Heading, Text } from "@radix-ui/themes";
+import { Flex } from "@radix-ui/themes";
 
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center">
+    <Flex className="justify-center items-center" direction="column">
       <Image
         src="/assets/RAG-banner.jpg"
-        width={500}
+        width={600}
         height={500}
         alt="Picture of the author"
       />
-    </div>
+      <Text className="italic">
+        Upload your documents and Talk to them with RAG!
+      </Text>
+    </Flex>
   );
 };
 
