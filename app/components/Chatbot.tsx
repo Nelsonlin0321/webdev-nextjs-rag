216b72e78e82ef53edd31bcab09d46211b868992
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-12-25 22:44:14 +0800

    Change chat record example

--- app/components/Chatbot.tsx
+++ app/components/Chatbot.tsx
@@ -19,10 +19,10 @@ export const Chatbot = ({ fileNames }: Props) => {
   const initChatRecords = [
     {
       question:
-        "Example: What are steps to take when finding projects to build your AI experience ?",
-      file_name: "eBook-How-to-Build-a-Career-in-AI.pdf",
+        "What is the percentage of total population in Hong Kong Island ? ",
+      file_name: "Hong Kong Fact Sheets - Population.pdf",
       answer:
-        "To find projects and build your AI experience, you can follow these steps:\n\n1. Identify a business problem: Start by finding a domain expert and ask them about the top three things they wish worked better. Focus on identifying a real-world problem rather than just an AI problem.\n\n2. Brainstorm AI solutions: Once you understand the problem, brainstorm potential AI solutions. Consider different approaches and technologies that can be applied to solve the problem effectively.\n\n3. Join existing projects: If someone else has an idea or ongoing project, ask to join them. Collaborating with others can provide valuable learning opportunities and help you gain practical experience.\n\n4. Keep reading and talking to people: Continuously educate yourself by reading books, taking courses, and engaging with domain experts. This can spark new ideas and expand your knowledge in AI.\n\n5. Focus on an application area: Instead of solely focusing on advancing basic AI technology, explore the vast variety of applications where machine learning has not yet been applied. Look for unique and creative opportunities to apply AI in specific domains.\n\nBy following these steps, you can find meaningful projects, gain experience, and build your portfolio in the field of AI.",
+        "At mid-2014, the population in Hong Kong Island accounted for 17.5% of the total population in Hong Kong.",
       uuid: "d1118d92-2ac5-4ba1-9735-6617578b01ac",
     },
   ];
